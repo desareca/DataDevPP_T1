3ac7efc1563f299c1e0939e0de26c6da4e657bc1
desareca <carlos.saquel@gmail.com>
2025-07-13 20:39:50 -0400

    Update predict.py

--- src/modeling/predict.py
+++ src/modeling/predict.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.metrics import classification_report, f1_score
+from sklearn.metrics import classification_report, f1_score, roc_auc_score, accuracy_score
 import joblib
 from src.config import RANDOM_STATE
 
@@ -16,9 +16,15 @@ def predict_proba(model, X):
 def evaluate_model(model, X, y):
     """Evaluate model and return metrics"""
     y_pred = predict_class(model, X)
+    y_pred_prob = predict_proba(model, X)[:, 1]
+    
     f1 = f1_score(y, y_pred)
+    auc = roc_auc_score(y, y_pred_prob)
+    acc = accuracy_score(y, y_pred)
+    mean_y = y.mean()
+    mean_ypred = y_pred.mean()
+    
     cr = classification_report(y, y_pred, output_dict=True)
     cr = pd.DataFrame(cr).transpose()
 
-    print(f"F1-Score: {f1:.3f}")
-    return f1, cr
+    return acc, f1, auc, mean_y, mean_ypred, cr
